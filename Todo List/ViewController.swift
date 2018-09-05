5b1a958a856770eacaf79c1aa5862dc262bf0aa5
Xuan Dat nguyen <datnx@nal.vn>
2018-09-05 08:15:42 +0700

    Initial Commit

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by Dat Nguyen on 9/5/18.
+//  Copyright © 2018 Dat Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
